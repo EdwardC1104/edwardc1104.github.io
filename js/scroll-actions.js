c7479e468ffd90c68e2a3fb7030f7f2d82acd814
EdwardC1104 <edwardclark1104@gmail.com>
2019-10-19 11:31:34 +0100

    Testing new scroll features. General fail

---
+++ js/scroll-actions.js
@@ -0,0 +1,19 @@
+/*
+$(function() {
+
+var controller = new ScrollMagic.Controller();
+
+new ScrollMagic.Scene({
+    triggerElement: "#scene-100%",
+//    duration: "100%"
+	duration: "100%"
+})
+//.setTween(".animation-spin", 0.5, {
+//    rotation: 360
+//})
+.setClassToggle("trigger", "drawn")
+.addIndicators() //DEBUGING
+.addTo(controller);
+
+});*/
+
