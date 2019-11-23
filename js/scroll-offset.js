b827972ef6f159281e198116713923ad4910599d
EdwardC1104 <edwardclark1104@gmail.com>
2019-11-23 10:44:01 +0000

    Smooth scrolling

--- js/scroll-offset.js
+++
@@ -1,7 +0,0 @@
-function offsetAnchor() {
-	if(location.hash.length !== 0) {
-		window.scrollTo(window.scrollX, window.scrollY - 71);
-	}
-}
-window.addEventListener("hashchange", offsetAnchor);
-window.setTimeout(offsetAnchor, 1);
