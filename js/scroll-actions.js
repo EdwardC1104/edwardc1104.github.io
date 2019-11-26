776906325899a79d312a232a654a76b8d292053b
EdwardC1104 <edwardclark1104@gmail.com>
2019-11-26 21:39:54 +0000

    Cogs

--- js/scroll-actions.js
+++ js/scroll-actions.js
@@ -10,8 +10,126 @@ const scene = new ScrollMagic.Scene({
 })
 	.setTween(tween)
 	.setPin(".animation1")
-	/*.addIndicators()*/
+	/**/
 	.addTo(controller);
+
+var tween2 = TweenLite.from( $('.cog'), .5,{rotation: 1080, ease:Quad.easeInOut});
+
+const controller2 = new ScrollMagic.Controller();
+
+const scene2 = new ScrollMagic.Scene({
+	triggerElement: "#cog",
+	duration: "100%",
+	triggerHook: 1
+})
+	.setTween(tween2)
+	
+	.addTo(controller2);
+
+const cog1 = new ScrollMagic.Scene({triggerElement: "#cog1", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog2 = new ScrollMagic.Scene({triggerElement: "#cog2", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog3 = new ScrollMagic.Scene({triggerElement: "#cog3", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog4 = new ScrollMagic.Scene({triggerElement: "#cog4", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog5 = new ScrollMagic.Scene({triggerElement: "#cog5", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog6 = new ScrollMagic.Scene({triggerElement: "#cog6", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog7 = new ScrollMagic.Scene({triggerElement: "#cog7", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog8 = new ScrollMagic.Scene({triggerElement: "#cog8", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog9 = new ScrollMagic.Scene({triggerElement: "#cog9", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog10 = new ScrollMagic.Scene({triggerElement: "#cog10", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog11 = new ScrollMagic.Scene({triggerElement: "#cog11", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog12 = new ScrollMagic.Scene({triggerElement: "#cog12", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog13 = new ScrollMagic.Scene({triggerElement: "#cog13", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog14 = new ScrollMagic.Scene({triggerElement: "#cog14", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog15 = new ScrollMagic.Scene({triggerElement: "#cog15", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog16 = new ScrollMagic.Scene({triggerElement: "#cog16", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog17 = new ScrollMagic.Scene({triggerElement: "#cog17", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog18 = new ScrollMagic.Scene({triggerElement: "#cog18", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog19 = new ScrollMagic.Scene({triggerElement: "#cog19", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog20 = new ScrollMagic.Scene({triggerElement: "#cog20", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog21 = new ScrollMagic.Scene({triggerElement: "#cog21", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog22 = new ScrollMagic.Scene({triggerElement: "#cog22", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog23 = new ScrollMagic.Scene({triggerElement: "#cog23", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog24 = new ScrollMagic.Scene({triggerElement: "#cog24", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog25 = new ScrollMagic.Scene({triggerElement: "#cog25", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog26 = new ScrollMagic.Scene({triggerElement: "#cog26", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog27 = new ScrollMagic.Scene({triggerElement: "#cog27", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog28 = new ScrollMagic.Scene({triggerElement: "#cog28", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog29 = new ScrollMagic.Scene({triggerElement: "#cog29", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog30 = new ScrollMagic.Scene({triggerElement: "#cog30", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog31 = new ScrollMagic.Scene({triggerElement: "#cog31", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog32 = new ScrollMagic.Scene({triggerElement: "#cog32", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog33 = new ScrollMagic.Scene({triggerElement: "#cog33", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog34 = new ScrollMagic.Scene({triggerElement: "#cog34", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog35 = new ScrollMagic.Scene({triggerElement: "#cog35", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog36 = new ScrollMagic.Scene({triggerElement: "#cog36", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog37 = new ScrollMagic.Scene({triggerElement: "#cog37", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog38 = new ScrollMagic.Scene({triggerElement: "#cog38", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog39 = new ScrollMagic.Scene({triggerElement: "#cog39", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog40 = new ScrollMagic.Scene({triggerElement: "#cog40", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog41 = new ScrollMagic.Scene({triggerElement: "#cog41", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog42 = new ScrollMagic.Scene({triggerElement: "#cog42", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog43 = new ScrollMagic.Scene({triggerElement: "#cog43", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog44 = new ScrollMagic.Scene({triggerElement: "#cog44", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog45 = new ScrollMagic.Scene({triggerElement: "#cog45", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog46 = new ScrollMagic.Scene({triggerElement: "#cog46", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog47 = new ScrollMagic.Scene({triggerElement: "#cog47", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog48 = new ScrollMagic.Scene({triggerElement: "#cog48", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog49 = new ScrollMagic.Scene({triggerElement: "#cog49", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog50 = new ScrollMagic.Scene({triggerElement: "#cog50", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog51 = new ScrollMagic.Scene({triggerElement: "#cog51", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog52 = new ScrollMagic.Scene({triggerElement: "#cog52", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog53 = new ScrollMagic.Scene({triggerElement: "#cog53", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog54 = new ScrollMagic.Scene({triggerElement: "#cog54", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog55 = new ScrollMagic.Scene({triggerElement: "#cog55", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog56 = new ScrollMagic.Scene({triggerElement: "#cog56", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog57 = new ScrollMagic.Scene({triggerElement: "#cog57", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog58 = new ScrollMagic.Scene({triggerElement: "#cog58", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog60 = new ScrollMagic.Scene({triggerElement: "#cog60", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog61 = new ScrollMagic.Scene({triggerElement: "#cog61", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog62 = new ScrollMagic.Scene({triggerElement: "#cog62", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog63 = new ScrollMagic.Scene({triggerElement: "#cog63", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog64 = new ScrollMagic.Scene({triggerElement: "#cog64", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog65 = new ScrollMagic.Scene({triggerElement: "#cog65", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog66 = new ScrollMagic.Scene({triggerElement: "#cog66", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog67 = new ScrollMagic.Scene({triggerElement: "#cog67", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog68 = new ScrollMagic.Scene({triggerElement: "#cog68", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog69 = new ScrollMagic.Scene({triggerElement: "#cog69", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog70 = new ScrollMagic.Scene({triggerElement: "#cog70", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog71 = new ScrollMagic.Scene({triggerElement: "#cog71", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog72 = new ScrollMagic.Scene({triggerElement: "#cog72", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog73 = new ScrollMagic.Scene({triggerElement: "#cog73", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog74 = new ScrollMagic.Scene({triggerElement: "#cog74", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog75 = new ScrollMagic.Scene({triggerElement: "#cog75", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog76 = new ScrollMagic.Scene({triggerElement: "#cog76", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog77 = new ScrollMagic.Scene({triggerElement: "#cog77", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog78 = new ScrollMagic.Scene({triggerElement: "#cog78", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog79 = new ScrollMagic.Scene({triggerElement: "#cog79", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog80 = new ScrollMagic.Scene({triggerElement: "#cog80", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog81 = new ScrollMagic.Scene({triggerElement: "#cog81", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog82 = new ScrollMagic.Scene({triggerElement: "#cog82", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog83 = new ScrollMagic.Scene({triggerElement: "#cog83", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog84 = new ScrollMagic.Scene({triggerElement: "#cog84", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog85 = new ScrollMagic.Scene({triggerElement: "#cog85", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog86 = new ScrollMagic.Scene({triggerElement: "#cog86", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog87 = new ScrollMagic.Scene({triggerElement: "#cog87", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog88 = new ScrollMagic.Scene({triggerElement: "#cog88", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog89 = new ScrollMagic.Scene({triggerElement: "#cog89", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog90 = new ScrollMagic.Scene({triggerElement: "#cog90", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog91 = new ScrollMagic.Scene({triggerElement: "#cog91", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog92 = new ScrollMagic.Scene({triggerElement: "#cog92", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog93 = new ScrollMagic.Scene({triggerElement: "#cog93", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog94 = new ScrollMagic.Scene({triggerElement: "#cog94", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog95 = new ScrollMagic.Scene({triggerElement: "#cog95", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog96 = new ScrollMagic.Scene({triggerElement: "#cog96", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog97 = new ScrollMagic.Scene({triggerElement: "#cog97", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog98 = new ScrollMagic.Scene({triggerElement: "#cog98", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+const cog99 = new ScrollMagic.Scene({triggerElement: "#cog99", duration: "100%", triggerHook: 1}).setTween(tween2).addTo(controller2);
+
+
+
+
+
+
 
 
 /*const LeftToRight = {
@@ -41,7 +159,7 @@ const scene = new ScrollMagic.Scene({
 
 })
 	.setTween(tween)
-	.addIndicators()
+	
 	.addTo(controller);*/
 
 
@@ -78,5 +196,5 @@ const scene = new ScrollMagic.Scene({
 
 })
 	.setTween(tween)
-	.addIndicators()
+	
 	.addTo(controller);*/
