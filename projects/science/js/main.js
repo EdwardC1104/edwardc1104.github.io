7d85064606d4df3f9117179a659f054d817e6a29
EdwardC1104 <edwardclark1104@gmail.com>
2020-04-11 22:14:56 +0100

    Look Upwards Section

--- projects/science/js/main.js
+++ projects/science/js/main.js
@@ -6,8 +6,8 @@ const flightPath = {
     curviness: 1.25,
     autoRotate: true,
     values: [
-        {x: (window.innerWidth * 0.25), y: (window.innerHeight * -0.02)},
-        {x: (window.innerWidth * 0.5), y: (window.innerHeight * 0.01)},
+        {x: (window.innerWidth * 0.2), y: (window.innerHeight * -0.04)},
+        {x: (window.innerWidth * 0.45), y: (window.innerHeight * 0.00)},
         {x: (window.innerWidth * 0.7 ), y: (window.innerHeight * 0.03)},
         {x: (window.innerWidth), y: (window.innerHeight * -0.05)}
     ]
@@ -25,11 +25,30 @@ tween.add(
 const controller = new ScrollMagic.Controller();
 
 const scene = new ScrollMagic.Scene({
-    triggerElement: ".reef-animation",
-    duration: 1500,
-    triggerHook: 0.75,
-    offset: 300
+    triggerElement: ".fish-space",
+    duration: window.innerHeight + 200,
+    triggerHook: 1,
 })
     .setTween(tween)
     //.addIndicators()
     .addTo(controller);
+
+
+
+
+
+
+
+    var tween2 = TweenLite.to(".look-upwards-text", 1, {className: "+=big-text"});
+
+    const controller2 = new ScrollMagic.Controller();
+
+    const scene2 = new ScrollMagic.Scene({
+    	triggerElement: ".look-upwards-section",
+    	duration: window.innerHeight,
+    	triggerHook: 1
+
+    })
+    	.setTween(tween2)
+    	//.addIndicators()
+    	.addTo(controller2);
